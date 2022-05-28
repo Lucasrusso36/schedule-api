f9d144ed9b406525ec05d525782a0bdeb7317113
Lucasrusso36 <lucaspidossantos@gmail.com>
2022-05-28 20:45:31 -0300

    table user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   has_secure_password
   #mount_uploader :avatar, AvatarUploader
   validates :username, presence: true, uniqueness: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password,
             length: { minimum: 6 },
             if: -> { new_record? || !password.nil? }
